51938a27a68371b387b6fb87aac1313969d68dcc
TODO-getajob <fer3@njit.edu>
2020-02-21 16:04:23 -0500

    Python messaging established.

--- Back-End/send.py
+++ Back-End/send.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import pika
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+# Establishes connect to RabbitMQ server
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='messaging'))
 channel = connection.channel()
-
+# Creates a queue
 channel.queue_declare(queue='hello')
-
+# routing_key specifies to which queue the message should go
 channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
 print(" [x] Sent 'Hello World!'")
 connection.close()
