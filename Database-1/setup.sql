c9792406ff26f08a3a4acd5a9a0fcff36dbbb441
svolcy7 <volcysamson@gmail.com>
2020-02-24 10:58:39 -0500

    database setup added to project

---
+++ Database-1/setup.sql
@@ -0,0 +1,3 @@
+create table users(user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,name TEXT,phone_number TEXT);
+INSERT INTO users (name,phone_number) values ('john','9738888888');
+INSERT INTO users (name,phone_number) values ('jean','9737800000');
