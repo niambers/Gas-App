3d352abe70724ef75a9cf4f5e9c0f55eb46c0b88
svolcy7 <volcysamson@gmail.com>
2020-03-02 10:28:29 -0500

    table has been altered

--- Database-1/setup.sql
+++ Database-1/setup.sql
@@ -1,3 +1,2 @@
-create table users(user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,name TEXT,phone_number TEXT);
-INSERT INTO users (name,phone_number) values ('john','9738888888');
-INSERT INTO users (name,phone_number) values ('jean','9737800000');
+create table users(user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,fname TEXT,lname TEXT,email TEXT,password TEXT);
+INSERT INTO users (fname,lname,email,password) values ('john','King','jking@gmail.com','jo1212');
