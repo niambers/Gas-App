258b5d6f230e6937a7256e76b2588ba0182ae87a
TODO-getajob <fer3@njit.edu>
2020-03-08 21:09:55 -0400

    Change to mysql 8.0

--- Back-End/test.php
+++ Back-End/test.php
@@ -1,7 +1,7 @@
 <?php
 
 
-error_reporting(E_ALL); // Displays all PHP errors and warnings
+
 
 $username = "test";
 $password = "test";
@@ -9,12 +9,3 @@ $hostname = "database";
 $dsn = "mysql:host=$hostname;dbname=$username"; // DO NOT ADD ANY SPACES, THROWS ERROR!
 
 $db = new PDO('mysql:host'$dsn, $username, $password);
-
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$fname = "john";
-$query = "SELECT * FROM users WHERE fname = :name";
-$statement = $db->prepare($query);
-$statement->bindValue(':name', $fname);
-$statement->bindValue(':password', $password);
-$statement->execute();
