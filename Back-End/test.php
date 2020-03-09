b70cf672069e2775237d3bdaf956caa94fa896f8
TODO-getajob <fer3@njit.edu>
2020-03-08 21:29:27 -0400

    Wrote PHP script to attempt connection to the database

--- Back-End/test.php
+++ Back-End/test.php
@@ -8,4 +8,10 @@ $password = "test";
 try {
     $connection = new PDO("mysql:host=$servername;$database",$username,$password);
     $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "Connection Successful";
 }
+catch (PDOException $e) {
+    echo "Connection Failed:" . $e->getMessage();
+}
+
+$connection = null; // Closes the connection automatically when the script ends
