4a224a01d52e584c34eadde8dc5f50d33b2ac4c5
TODO-getajob <fer3@njit.edu>
2020-03-13 12:56:43 -0400

    Established connection to DB and inserted test data

--- Back-End/test.php
+++ Back-End/test.php
@@ -1,20 +1,27 @@
 <?php
-
-$test = "Hello World!!\n";
+$test = "TEST\n";
 echo $test;
-
-$servername = "database";
+$servername = "database"; // Automatically resolves to the docker service named 'database'
 $database = "test";
 $username = "test";
 $password = "test";
+$attempt = 3;
 
-try {
-    $connection = new PDO("mysql:host=$servername;port=3306,$database",$username,$password);
-    $connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    echo "Connection Successful";
-}
-catch (PDOException $e) {
-    echo "Connection Failed:" . $e->getMessage();
-}
+while ($attempt>0) { // Attempts to connect to the database 3 times
+    try {
+        sleep(2); // Wait 2 seconds before attempting to connect to the database
+        $connection = new PDO("mysql:host=$servername;dbname=test",$username,$password);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set PDO error mode to exception
+        $query = "INSERT INTO users (user_id,name,phone_number) VALUES ('3','joe','9333339345')";
+        $connection->exec($query);
+        $attempt = 0;
+        echo "Connection Successful\n";
+    }
 
-//$connection = null; // Closes the connection automatically when the script ends
+    catch (PDOException $e) {
+        echo "Connection Failed: " . $e->getMessage() . "\n"; // Print out error messages
+        $attempt--;
+        sleep(2); // Wait 2 seconds between attempts
+    }
+}
+//$connection = null; // Closes the connection automatically when the program ends
