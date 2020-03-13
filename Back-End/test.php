032cc129199e180f8efd1b81bfa72b6024abe68a
TODO-getajob <fer3@njit.edu>
2020-03-13 13:04:26 -0400

    Cleaned up test.php test code

--- Back-End/test.php
+++ Back-End/test.php
@@ -1,6 +1,5 @@
 <?php
-$test = "TEST\n";
-echo $test;
+
 $servername = "database"; // Automatically resolves to the docker service named 'database'
 $database = "test";
 $username = "test";
@@ -12,12 +11,9 @@ while ($attempt>0) { // Attempts to connect to the database 3 times
         sleep(2); // Wait 2 seconds before attempting to connect to the database
         $connection = new PDO("mysql:host=$servername;dbname=test",$username,$password);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set PDO error mode to exception
-        $query = "INSERT INTO users (user_id,name,phone_number) VALUES ('3','joe','9333339345')";
-        $connection->exec($query);
-        $attempt = 0;
+        $attempt = 0; // Set attempt to 0 to exit while loop after successful connection
         echo "Connection Successful\n";
     }
-
     catch (PDOException $e) {
         echo "Connection Failed: " . $e->getMessage() . "\n"; // Print out error messages
         $attempt--;
@@ -25,3 +21,5 @@ while ($attempt>0) { // Attempts to connect to the database 3 times
     }
 }
 //$connection = null; // Closes the connection automatically when the program ends
+//$query = "INSERT INTO users (user_id,name,phone_number) VALUES ('3','joe','9333339345')";
+//$connection->exec($query);
