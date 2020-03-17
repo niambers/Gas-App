5ede0c29c811b7b2441ecc492c29ade32d300552
TODO-getajob <fer3@njit.edu>
2020-03-17 19:49:25 -0400

    Back-End RabbitMQ messaging established.

--- Back-End/test.php
+++ Back-End/test.php
@@ -1,11 +1,10 @@
 <?php
-// Testing RabbitMQ receiving code
-require_once __DIR__ . '/vendor/autoload.php';
+require 'vendor/autoload.php';
+
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 $connection = new AMQPStreamConnection('messaging', 5672, 'guest', 'guest');
 $channel = $connection->channel();
-
 $channel->queue_declare('hello', false, false, false, false);
 
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
